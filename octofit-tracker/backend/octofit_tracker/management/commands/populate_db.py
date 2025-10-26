6c9b0074a7acf4722aea284298e9caa92fe0d039
luisfe12 <42527787+luisfe12@users.noreply.github.com>
2025-10-26 05:22:23 +0000

    Agregar comando para poblar la base de datos octofit_db con datos de prueba de superhéroes, equipos y actividades.

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Borrar datos previos
+        Activity.objects.all().delete()
+        Workout.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+
+        # Crear equipos
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Crear usuarios superhéroes
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User.objects.create(name='Captain America', email='cap@marvel.com', team=marvel),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc),
+            User.objects.create(name='Superman', email='superman@dc.com', team=dc),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+        ]
+
+        # Crear actividades
+        Activity.objects.create(user=users[0], type='Running', duration=30, date='2025-10-25')
+        Activity.objects.create(user=users[3], type='Cycling', duration=45, date='2025-10-25')
+
+        # Crear workouts
+        w1 = Workout.objects.create(name='Full Body', description='Entrenamiento completo')
+        w2 = Workout.objects.create(name='Cardio', description='Entrenamiento cardiovascular')
+        w1.suggested_for.set(users[:3])
+        w2.suggested_for.set(users[3:])
+
+        # Crear leaderboard
+        Leaderboard.objects.create(team=marvel, points=100)
+        Leaderboard.objects.create(team=dc, points=120)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db poblada con datos de prueba (superhéroes, equipos Marvel y DC)'))
